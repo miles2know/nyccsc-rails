f46a086cbf5fd068f7eeb864664f4bca0cf2dd44
Darsy Branchini <dad284@cornell.edu>
2014-05-22 11:06:25 -0400

    url_for_document added to application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+	def url_for_document doc, options = {}
+    if respond_to?(:blacklight_config) and
+        blacklight_config.show.route and
+        (!doc.respond_to?(:to_model) or doc.to_model.is_a? SolrDocument)
+      route = blacklight_config.show.route.merge(action: :show, id: doc).merge(options)
+      route[:controller] = controller_name if route[:controller] == :current
+      route
+    else
+       CGI.escapeHTML(doc.id)
+      #doc
+      # "google.com"
+      # Rails.logger.info "////////////// ENCODE URI #{CGI::escape(doc.id)} ENCODE URI ///////////////"
+    end
+  end
+
 end
