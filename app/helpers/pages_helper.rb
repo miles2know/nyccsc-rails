26992ae9cef50a3174739c6678a26b1b7b6b29e2
Darcy Branchini <dad284@cornell.edu>
2014-07-10 15:39:18 -0400

    Added documentation to PagesHelper

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,8 @@
 module PagesHelper 
 
+  #this is just a test - using the url path to determine parameter values
+  # in this case /year/month and month is optional
+
   def calendar (month, year)
     prev_month = link_to "previous", calendar_path(:month => month.to_i - 1, :year => year)
     cal_str =  "<table border='1'>\n"
