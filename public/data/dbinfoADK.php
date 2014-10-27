bc1cab5dfc572d3ace691b58930057318182c3dc
Darcy Branchini <dad284@cornell.edu>
2014-10-27 15:49:46 -0400

    delete extra map files accessed through frontierspatial.com

--- public/data/dbinfoADK.php
+++
@@ -1,7 +0,0 @@
-<?php
-$user="readonly";
-$password="r3@d*nly";
-$database="adk";
-$host="frontierspatial.com";
-$port="5432";
-?>
--- public/data/dbinfoNY.php
+++
@@ -1,7 +0,0 @@
-<?php
-$user="readonly";
-$password="r3@d*nly";
-$database="ny";
-$host="forestecoservices.net";
-$port="5432";
-?>
