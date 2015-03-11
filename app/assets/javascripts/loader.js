faf0c396d53e8e77415ba14f6c748439be0a18fb
Bill Noon <wn10@cornell.edu>
2015-03-11 16:08:14 -0400

    missed the loader.js file update

---
+++ app/assets/javascripts/loader.js
@@ -0,0 +1,47 @@
+webpackJsonp([0],{
+
+/***/ 0:
+/*!******************!*\
+  !*** multi main ***!
+  \******************/
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(/*! ./src/loader */5);
+
+
+/***/ },
+
+/***/ 5:
+/*!***********************!*\
+  !*** ./src/loader.js ***!
+  \***********************/
+/***/ function(module, exports, __webpack_require__) {
+
+	/* REACT HOT LOADER */ if (false) { (function () { var ReactHotAPI = require("/Users/noon/js/rails-fixture/node_modules/react-hot-loader/node_modules/react-hot-api/modules/index.js"), RootInstanceProvider = require("/Users/noon/js/rails-fixture/node_modules/react-hot-loader/RootInstanceProvider.js"), ReactMount = require("react/lib/ReactMount"), React = require("react"); module.makeHot = module.hot.data ? module.hot.data.makeHot : ReactHotAPI(function () { return RootInstanceProvider.getRootInstances(ReactMount); }, React); })(); } (function () {
+	
+	"use strict";
+	
+	window.extensionDefs = {
+	  n12089: __webpack_require__(/*! bundle?lazy!./docJS/n12089 */ 13)
+	};
+	
+	/* REACT HOT LOADER */ })(); if (false) { (function () { module.hot.dispose(function (data) { data.makeHot = module.makeHot; }); if (module.exports && module.makeHot) { var makeExportsHot = require("/Users/noon/js/rails-fixture/node_modules/react-hot-loader/makeExportsHot.js"), foundReactClasses = false; if (makeExportsHot(module, require("react"))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot not apply hot update to " + "loader.js" + ": " + err.message); } }); } } })(); }
+
+/***/ },
+
+/***/ 13:
+/*!****************************************************!*\
+  !*** ./~/bundle-loader?lazy!./src/docJS/n12089.js ***!
+  \****************************************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = function(cb) {
+		__webpack_require__.e/* nsure */(1, function(require) {
+			cb(__webpack_require__(/*! !./~/react-hot-loader!./~/babel-loader!./src/docJS/n12089.js */ 14));
+		});
+	}
+
+/***/ }
+
+});
+//# sourceMappingURL=loader.js.map
